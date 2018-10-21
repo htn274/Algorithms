7504f3aa7d9bfab2cbbc7605f7ba551cef241f6b
htn274 <thiennust274@gmail.com>
2018-10-21 22:38:00 +0700

    Update sorting
    Sorting implementation

---
+++ Sorting/Sorting.h
@@ -0,0 +1,173 @@
+#pragma once
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_idx;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		// Find the minimum element in unsorted array
+		min_idx = i;
+		for (j = i + 1; j < n; j++)
+			if (arr[j] < arr[min_idx])
+				min_idx = j;
+		// Swap the found minimum element with the first element
+		swap(&arr[min_idx], &arr[i]);
+	}
+}
+
+void bubbleSort(int arr[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++)
+
+		// Last i elements are already in place   
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(&arr[j], &arr[j + 1]);
+}
+
+void ShakerSort(int a[], int n)
+{
+	int Left = 0;
+	int Right = n - 1;
+	int k = 0;
+	while (Left < Right)
+	{
+		for (int i = Left; i < Right; i++)
+		{
+			if (a[i] > a[i + 1])
+			{
+				swap(&a[i], &a[i + 1]);
+				k = i;
+			}
+		}
+		Right = k;
+		for (int i = Right; i > Left; i--)
+		{
+			if (a[i] < a[i - 1])
+			{
+				swap(&a[i], &a[i - 1]);
+				k = i;
+			}
+		}
+		Left = k;
+	}
+}
+
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+
+		/* Move elements of arr[0..i-1], that are
+		greater than key, to one position ahead
+		of their current position */
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high];    // pivot
+	int i = (low - 1);  // Index of smaller element
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;    
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high);
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	/* create temp arrays */
+	int *L, *R;
+	L = new int[n1];
+	R = new int[n2];
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; 
+	j = 0; 
+	k = l; 
+	while (i < n1 && j < n2){
+		if (L[i] <= R[j]){
+			arr[k] = L[i];
+			i++;
+		}
+		else{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	/* Copy the remaining elements of L[], if there
+	are any */
+	while (i < n1){
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	/* Copy the remaining elements of R[], if there
+	are any */
+	while (j < n2){
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r)
+	{
+		int m = l + (r - l) / 2;
+
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		merge(arr, l, m, r);
+	}
+}
